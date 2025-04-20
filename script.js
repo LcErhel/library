c74499a0a6cac09157afcfb663a033ad223934e8
project <132855195+LcErhel@users.noreply.github.com>
2025-04-20 14:31:36 +0300

    add Book constructor
    add fuctions addBookToLibrary, createBook

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const myLibrary = [];
+
+function Book(name, author, pages, read) {
+    this.id = crypto.randomUUID();
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    let newBook = createBook();
+    myLibrary.push(newBook);
+    console.log(newBook);
+}
+
+function createBook() {
+    let name = prompt("Book name");
+    let author = prompt("Author");
+    let pages = prompt("Amount of pages");
+    let read = false;
+    return new Book(name, author, pages, read);
+}
