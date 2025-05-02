deba70dbbb8e99dcec9cab0ff4ace85456cdc24f
project <132855195+LcErhel@users.noreply.github.com>
2025-05-02 19:38:38 +0300

    add refresh()

--- script.js
+++ script.js
@@ -14,3 +14,28 @@ function addBookToLibrary(name = "name", author = "author", pages = 50, read = f
     let newBook = new Book(name, author, pages, read);
     return myLibrary.push(newBook);
 }
+
+addBookToLibrary();
+addBookToLibrary("namae", "jjr martin", "1654", true);
+
+const display = document.querySelector(".display");
+
+function refresh() {
+    myLibrary.forEach(book => {
+        let newItem = document.createElement("div");
+        newItem.classList.add("book");
+        newItem.textContent = `${book.name}, ${book.author}, ${book.pages}, ${book.read}`;
+    
+        let deleteBtn = document.createElement("div");
+        deleteBtn.classList.add("btn");
+        deleteBtn.textContent = "delete";
+        deleteBtn.addEventListener("click", () => {
+            deleteBtn.parentElement.remove();
+        });
+        newItem.appendChild(deleteBtn);
+    
+        display.appendChild(newItem);
+    });
+}
+
+refresh();
